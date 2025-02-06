15f91c5c6d020b30b583597446bb5b9d4b709752
osuen1 <st.akimov1@gmail.com>
2025-02-06 11:26:12 +0300

    added module keyboard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, ttk
+import keyboard
 
 #TODO
 # сделать контекстные подсказки по командам
@@ -15,6 +16,8 @@ text_area = ttk.Entry()
 text_area.focus()
 text_area.pack(fill='x', padx=20, pady=20)
 
+if text_area.get() != None:
+    keyboard.add_hotkey("f1+enter", lambda: print("hello"))
 
 if __name__ == '__main__':
     root.mainloop()
